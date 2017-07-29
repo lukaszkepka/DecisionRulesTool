1a68d713663ce67767a382569fbe3315fc3301a2
lukaszkepka <lukaszkepka95@gmail.com>
2017-07-29 19:02:41 +0200

    Solution structure update

---
+++ DecisionRulesTool/DecisionRulesTool.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using DecisionRulesTool.Model;
+using DecisionRulesTool.Model.Model;
+using DecisionRulesTool.Model.Parsers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionRulesTool.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string _4EmkaRulesFilePath = "/Examples/RSES/Rules/male.rul";
+            IFileParser<RulesSet> r = new RsesRulesParser();
+
+            r.ParseFile(Globals.TestFilesDirectory + _4EmkaRulesFilePath);
+        }
+    }
+}
